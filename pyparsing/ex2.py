77a0a804f7ff0f3cf0c9411a4702445b4bab5bb0
vagrant <vagrant@debian>
2016-06-23 06:24:31 +0200

    add test_evalStack

--- pyparsing/ex2.py
+++ pyparsing/ex2.py
@@ -20,23 +20,36 @@ op = add | sub | mul | div
 atom = Word(nums).addParseAction(pushFirst)
 expr = atom + ZeroOrMore((op + atom).addParseAction(pushFirst))
 
-def evalString(s):
+def parseString(s):
     global exprStack
     exprStack = []
     expr.parseString(s)
     return exprStack
 
+opf = { '+' : (lambda a, b: a + b),
+        '-' : (lambda a, b: a - b),
+        '*' : (lambda a, b: a * b),
+        '/' : (lambda a, b: a / b) }
+
+def evalStack(s):
+    op = s.pop()
+    if op in '+-*/':
+        op2 = evalStack(s)
+        op1 = evalStack(s)
+        return opf[op](op1, op2)
+    else:
+        return float(op)
+
 class TestEx2(unittest.TestCase):
 
-    def test_single_op(self):
-        self.assertEqual(evalString('6+3'), ['6', '3', '+'])
-        self.assertEqual(evalString('6-3'), ['6', '3', '-'])
-        self.assertEqual(evalString('6*3'), ['6', '3', '*'])
-        self.assertEqual(evalString('6/3'), ['6', '3', '/'])
+    def test_parseString(self):
+        self.assertEqual(parseString('6+3'), ['6', '3', '+'])
+        self.assertEqual(parseString('6-3'), ['6', '3', '-'])
+        self.assertEqual(parseString('6*3'), ['6', '3', '*'])
+        self.assertEqual(parseString('6/3'), ['6', '3', '/'])
 
-    def test_multiple_op(self):
-        self.assertEqual(evalString('6+3+2'), ['6', '3', '+', '2', '+'])
-        self.assertEqual(evalString('6-3-2'), ['6', '3', '-', '2', '-'])
-        self.assertEqual(evalString('6*3*2'), ['6', '3', '*', '2', '*'])
-        self.assertEqual(evalString('6/3/2'), ['6', '3', '/', '2', '/'])
-
+    def test_evalStack(self):
+        self.assertEqual(evalStack(['6', '3', '+']), 9.0)
+        self.assertEqual(evalStack(['6', '3', '-']), 3.0)
+        self.assertEqual(evalStack(['6', '3', '*']), 18.0)
+        self.assertEqual(evalStack(['6', '3', '/']), 2.0)
